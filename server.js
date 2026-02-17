3283925bf5f675e1389391df0773be68248e542c
dwiky106 <167574876+dwiky106@users.noreply.github.com>
2026-02-17 16:44:16 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const SECRET = "rahasia";
+
+const user = {
+  username: "admin",
+  password: "123"
+};
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === user.username && password === user.password) {
+    const token = jwt.sign({ username }, SECRET);
+    res.json({ success: true, token });
+  } else {
+    res.json({ success: false });
+  }
+});
+
+app.listen(3000, () => console.log("Server jalan"));
